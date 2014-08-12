f8d605c1038836c6d78f3ebbf2b30aa0ffbd78d0
slamice <slamice@gmail.com>
2014-08-13 00:26:59 +0900

    added jquery and typo.js and simple debug

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+
+/*  var checkTypo = function($target) {
+    console.log('something works?');
+  };*/
+
+  // ------ Translator side ------------------------------------------------------------------
+  // Single page details
+  //checkTypo( $('#comment-body').find('p') );
+  alert("another gah");
+}
