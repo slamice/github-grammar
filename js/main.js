8d8abc6bb04ec7a6df8de2a0aa2e3feca77ba10a
slamice <slamice@gmail.com>
2014-08-21 00:32:07 +0900

    added poor textarea highlight, doesnt work yet

--- js/main.js
+++ js/main.js
@@ -4,34 +4,40 @@ $(function() {
   var lightblue  = '#A9D0F5';
   var lightred   = '#F5A9A9';
 
-  var highlightSpellingError = function($target, $color) {
-    console.log($target);
+  var highlightSpellingError = function($target, $color, $ele_id) {
+    console.log($ele_id);
+    $($ele_id).highlightTextarea({
+      words: [{
+          color: $color,
+          words: [$target]
+        }]
+    });
 
 //    if ( /(\{\/?\d+\})/g.test(html) ) {
-      html = $target.replace($target, '<span style=\"background-color: #' + $color + ';\">$1</span>');
-      $target.html( html );
-      console.log($target.html());
+      // html = $target.replace($target, '<span style=\"background-color: #' + $color + ';\">$1</span>');
+      // $target.html( html );
+      // console.log($target.html());
 //    }
   };
 
-  var addHiglighting = function($target, $color) {
+  var addHiglighting = function($target, $color, $ele_id) {
     if ($target.length !== 0) {
-      highlightSpellingError( $target , $color);
+      highlightSpellingError( $target , $color, $ele_id);
     }
   };
 
-  $('#new_comment_field').each(function(){
+  $('textarea').each(function(){
+
     var words = $(this).val().split(' ');
-    
+    var ele_id = $(this).attr('id');
     var i = 0, item;
     for ( ; item = words[i++] ; ){
 
       var is_spelled_correctly = dictionary.check(item);
       if(!is_spelled_correctly){
         //console.log('theText');
-        addHiglighting(item, '#A9D0F5');
+        addHiglighting(item, '#A9D0F5', ele_id);
       }
-
     }
 
   });
