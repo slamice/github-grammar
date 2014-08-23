db53e8007aeb7aedcd5ac6e3bea2814b4a995400
slamice <slamice@gmail.com>
2014-08-24 00:43:47 +0900

    added more debug, hiilighting kind of works

--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ $(function() {
   var lightred   = '#F5A9A9';
 
   var highlightSpellingError = function($target, $color, $ele_id) {
-    console.log($ele_id);
-    $($ele_id).highlightTextarea({
-      words: [{
-          color: $color,
-          words: [$target]
-        }]
+    console.log('element: '+$ele_id);
+    console.log('target: '+$target);
+    $('textarea').highlightTextarea({
+      words: $target
     });
 
 //    if ( /(\{\/?\d+\})/g.test(html) ) {
@@ -28,17 +26,19 @@ $(function() {
 
   $('textarea').each(function(){
 
+    var word_list = []
     var words = $(this).val().split(' ');
     var ele_id = $(this).attr('id');
     var i = 0, item;
     for ( ; item = words[i++] ; ){
 
       var is_spelled_correctly = dictionary.check(item);
-      if(!is_spelled_correctly){
+      if(!is_spelled_correctly && item != ''){
         //console.log('theText');
-        addHiglighting(item, '#A9D0F5', ele_id);
+        word_list.push(item);
       }
     }
+    addHiglighting(word_list, '#A9D0F5', ele_id);
 
   });
 
